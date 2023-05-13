02748af60034c04eb0e9fd969ae155457ce123a5
decayatf <mrmaximko97@gmail.com>
2023-05-14 01:23:11 +1000

    OK

---
+++ c_labs/3_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void main() {
+    char str[81];
+    printf("Введите строку символов (до 80 символов): ");
+    fgets(str, 81, stdin); // считываем строку с консоли
+    
+    int len = strlen(str);
+    for (int i = 0; i < len; i++) {
+        if (str[i] == 'a' || str[i] == 'b') { // если символ маленький 'a' или 'b'
+            str[i] = toupper(str[i]); // меняем на заглавный символ 
+        }
+    }
+    
+    printf("Результат: %s", str);
+}
